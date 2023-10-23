1a67ad2d1ebdc8c17df5307a6eef32112a1423a6
MarianaMTF <142687755+MarianaMTF@users.noreply.github.com>
2023-10-23 10:40:29 -0300

    Create atividade8.py

---
+++ atividade8.py
@@ -0,0 +1,9 @@
+numero1=input("digite um numero:")
+numero2=input("digite outro numero:")
+numero3=input("digite outro numero:")
+if numero1>numero2 and numero1>numero3:
+  print(f"{numero1} é maior")
+elif numero2>numero1 and numero2>numero3:
+  print(f"{numero2} é o maior")
+else:
+  print(f"{numero3 é o maior}")
