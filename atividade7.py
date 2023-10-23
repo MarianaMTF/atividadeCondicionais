a707e04b84a8cb58d77f47d74a53a59f478938e9
MarianaMTF <142687755+MarianaMTF@users.noreply.github.com>
2023-10-23 10:38:03 -0300

    Create atividade7.py

---
+++ atividade7.py
@@ -0,0 +1,10 @@
+x=input("digite o valor de x:")
+y=input("digite o valor de y:")
+if x>0 and y>0:
+  print("primeiro quadrante")
+elif x<0 and y>0:
+  print("segundo quadrante")
+elif x<0 and y<0:
+  print("terceiro quadrante")
+elif x>0 and y<0:
+  print("quarto quadrante")
